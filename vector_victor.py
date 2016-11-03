4ffd43126c7ceabb8bec4bf2a465d5b559eb9631
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-11-02 20:39:46 -0400

    Add files via upload

---
+++ vector_victor.py
@@ -0,0 +1,41 @@
+import math
+
+class ShapeError():
+    pass
+
+def shape(vector_list):
+    l = list(vector_list)
+    return (len(l), )
+
+def vector_add(self, other):
+    if len(self) == len(other):
+        return [x + y for x, y in zip(self, other)]
+    else:
+        raise ShapeError()
+
+def vector_sub(self, other):
+    if len(self) == len(other):
+        return [x - y for x, y in zip(self, other)]
+    else:
+        raise ShapeError()
+
+def vector_sum(*args):
+    if len(set([shape(x) for x in args])) == 1:
+        return [sum(x) for x in zip(*args)]
+    else:
+        raise ShapeError()
+
+def dot(self, other):
+    if len(self) == len(other):
+        return sum([x * y for x, y in zip(self, other)])
+    else:
+        raise ShapeError()
+
+def vector_multiply(self, mult):
+    return [x * mult for x in self]
+
+def vector_mean(*args):
+    return [sum(x)/float(len(args)) for x in zip(*args)]
+
+def magnitude(self):
+    return math.sqrt(sum(i**2 for i in self))
