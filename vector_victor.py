48a20780c24bcb0c7dc375b7f087c47abb9afcf5
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-12-06 21:49:41 -0500

    Update vector_victor.py

--- vector_victor.py
+++ vector_victor.py
@@ -1,23 +1,32 @@
 import math
 
+
 class ShapeError(Exception):
     pass
 
+
 def shape(vector_list):
-    l = list(vector_list)
-    return (len(l), )
+    if type(vector_list[0]) == list:
+        matrix_size = tuple([len(vector_list), len(vector_list[0])])
+        return matrix_size
+    else:
+        l = list(vector_list)
+        return (len(l), )
 
-def vector_add(self, other):
-    if len(self) == len(other):
-        return [x + y for x, y in zip(self, other)]
+
+def vector_add(m, n):
+    if len(m) == len(n):
+        return [x + y for x, y in zip(m, n)]
     else:
         raise ShapeError(Exception)
 
-def vector_sub(self, other):
-    if len(self) == len(other):
-        return [x - y for x, y in zip(self, other)]
+
+def vector_sub(m, n):
+    if len(m) == len(n):
+        return [x - y for x, y in zip(m, n)]
     else:
         raise ShapeError(Exception)
+
 
 def vector_sum(*args):
     if len(set([shape(x) for x in args])) == 1:
@@ -25,17 +34,21 @@ def vector_sum(*args):
     else:
         raise ShapeError(Exception)
 
-def dot(self, other):
-    if len(self) == len(other):
-        return sum([x * y for x, y in zip(self, other)])
+
+def dot(m, n):
+    if len(m) == len(n):
+        return sum([x * y for x, y in zip(m, n)])
     else:
         raise ShapeError(Exception)
 
-def vector_multiply(self, mult):
-    return [x * mult for x in self]
+
+def vector_multiply(m, mult):
+    return [x * mult for x in m]
+
 
 def vector_mean(*args):
-    return [sum(x)/float(len(args)) for x in zip(*args)]
+    return [sum(x)/(len(x)) for x in zip(*args)]
 
-def magnitude(self):
-    return math.sqrt(sum(i**2 for i in self))
+
+def magnitude(m):
+    return math.sqrt(sum(i**2 for i in m))
